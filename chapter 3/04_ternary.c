1be0f4b67ddd47d847e95c2489a6a961a7ed49da
aryan2729 <aryancyber282h@gmail.com>
2024-12-22 00:39:24 +0530

    chapter 3

---
+++ chapter 3/04_ternary.c
@@ -0,0 +1,19 @@
+/* CONDITIONAL OPERATORS
+ A shorthand “if – else” can be written
+ using the conditional or ternary operators 
+ condition ? expression-if-true : expression-if-false  .*/
+#include<stdio.h>
+
+int main(){
+    
+    // condition ? expression-if-true : expression-if-false 
+
+    int a = 345 , b = 34;
+
+    a > b ? printf("a is greater than b") : printf("a is lesser than b");
+     
+    
+
+    return 0;
+}
+ 
---
+++ chapter 3/05_switch.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+
+    int a;
+    printf("Enter the value of a under 4: ");
+
+    scanf("%d" , &a);
+
+    switch (a)
+    {
+    case 1:
+        printf("You entered 1");
+        break;
+    case 2:
+        printf("You entered 2");
+        break;
+    case 3:
+        printf("You entered 3");
+        break;
+    case 4:
+        printf("You entered 4");
+        break;
+    
+    default:
+        break;
+    }
+
+    return 0;
+}
